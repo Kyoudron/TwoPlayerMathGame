79b59945aab88cd3752560cd80f973ee5c64a1c1
Kapish <kapish.k.1@gmail.com>
2017-02-13 17:48:01 -0500

    Fully function math game

--- game.rb
+++ game.rb
@@ -1,4 +1,38 @@
 require_relative 'player'
 require_relative 'numbermaker'
 
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
 
+
+
+def player_turn(player)
+  question =  Question.new
+  puts "What is #{question.random_number1} plus #{question.random_number2}?"
+
+  player_guess = gets.chomp
+
+  if (player_guess.to_i == (question.random_number1 + question.random_number2))
+    puts "Correct Answer!"
+  else
+    puts "Wrong Answer!"
+    player.wrong
+  end
+
+
+end
+
+loop do
+  player_turn(player1)
+  puts "#{player1.name} has #{player1.life}/3 and #{player2.name} has #{player2.life}/3"
+  break puts "Player 2 wins with a final score of #{player2.life}/3" if(player1.lose)
+
+  player_turn(player2)
+  puts "#{player1.name} has #{player1.life}/3 and #{player2.name} has #{player2.life}/3"
+
+  break puts "Player 1 wins with a final score of #{player1.life}/3" if(player2.lose)
+
+  puts "------ NEW TURN ------"
+end
+  puts "------ GAME OVER ------"
+  puts "Good bye!"
--- numbermaker.rb
+++ numbermaker.rb
@@ -1,12 +1,8 @@
 class Question
   attr_reader :random_number1, :random_number2
 
-  def self.random_number1
+  def initialize
     @random_number1 = 1 + rand(19)
-  end
-
-  def self.random_number2
     @random_number2 = 1 + rand(19)
   end
-
 end
